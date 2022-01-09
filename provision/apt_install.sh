a3e6efd2bb188017916edce9da72d32bc3ab9a85
blitterated <blitterated@protonmail.com>
2022-01-09 13:38:55 -0600

    added tmux to image

--- provision/apt_install.sh
+++ provision/apt_install.sh
@@ -1,6 +1,6 @@
 apt --yes install tree gzip tar curl \
-          bash-completion \
-          git silversearcher-ag vim
+          bash-completion vim tmux \
+          git silversearcher-ag
 
 # for some reason, man pages for these don't install on first pass
 apt --reinstall --yes install gzip tar
