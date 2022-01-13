2649d825f179fdac6e59aeed85401c54c8d7b789
blitterated <blitterated@protonmail.com>
2022-01-13 10:45:31 -0600

    added reinstall of bash to get its man pages

--- provision/apt_install.sh
+++ provision/apt_install.sh
@@ -2,5 +2,6 @@ apt --yes install tree gzip tar curl \
           bash-completion vim tmux \
           git silversearcher-ag
 
-# for some reason, man pages for these don't install on first pass
-apt --reinstall --yes install gzip tar
+# for some reason, man pages for these don't install on first pass.
+# or they don't get installe for certain preinstalled packages.
+apt --reinstall --yes install gzip tar bash
