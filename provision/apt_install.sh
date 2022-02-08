49fe6b3cf46f33e981fb7311eb2f6f3745507d05
blitterated <blitterated@protonmail.com>
2022-02-08 17:40:23 -0600

    typo

--- provision/apt_install.sh
+++ provision/apt_install.sh
@@ -3,5 +3,5 @@ apt --yes install tree gzip tar xz-utils \
           git silversearcher-ag
 
 # for some reason, man pages for these don't install on first pass.
-# or they don't get installe for certain preinstalled packages.
+# or they don't get installed for certain preinstalled packages.
 apt --reinstall --yes install gzip tar bash
