600e2e87b4b2b50ad7115f95642fe72e02da8966
blitterated <blitterated@protonmail.com>
2022-03-08 19:16:33 -0600

    Added psmisc and htop to install

--- provision/apt_install.sh
+++ provision/apt_install.sh
@@ -1,6 +1,9 @@
 apt --yes install gzip tar xz-utils unzip \
           curl bash-completion ed vim tmux \
-          tree git silversearcher-ag
+          psmisc tree htop \
+          git silversearcher-ag
+
+# NOTE: psmisc is a collection of process utils inc. pstree.
 
 # for some reason, man pages for these don't install on first pass.
 # or they don't get installed for certain preinstalled packages.
