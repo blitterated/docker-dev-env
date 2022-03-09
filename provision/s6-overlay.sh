56d4df585b318dd0419617a6b29f8b63d46a8cb8
blitterated <blitterated@protonmail.com>
2022-03-09 13:43:34 -0600

    Upgrade to s6 3.1.0.1

--- provision/s6-overlay.sh
+++ provision/s6-overlay.sh
@@ -1,8 +1,11 @@
-S6_OVERLAY_VERSION=3.0.0.2
+S6_OVERLAY_VERSION=3.1.0.1
 
-(cd /tmp && curl -LO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-noarch-${S6_OVERLAY_VERSION}.tar.xz")
-tar -C / -Jxpf /tmp/s6-overlay-noarch-${S6_OVERLAY_VERSION}.tar.xz
-(cd /tmp && curl -LO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-x86_64-${S6_OVERLAY_VERSION}.tar.xz")
-tar -C / -Jxpf /tmp/s6-overlay-x86_64-${S6_OVERLAY_VERSION}.tar.xz
+# Using parens with `cd` and `curl` to temporarily change working directory in a subshell.
+# This allows us to use `curl` to download to a different directory than the current one.
+
+(cd /tmp && curl -LO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-noarch.tar.xz")
+tar -C / -Jxpf /tmp/s6-overlay-noarch.tar.xz
+(cd /tmp && curl -LO "https://github.com/just-containers/s6-overlay/releases/download/v${S6_OVERLAY_VERSION}/s6-overlay-x86_64.tar.xz")
+tar -C / -Jxpf /tmp/s6-overlay-x86_64.tar.xz
 
 rm -rf /tmp/*
