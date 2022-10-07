96b372c76759884b9afe4401e41d8ea7179c4d07
blitterated <blitterated@protonmail.com>
2022-10-07 13:04:05 -0500

    Shell config sourced from discrete files by .bashrc

--- Dockerfile
+++ Dockerfile
@@ -3,17 +3,19 @@ FROM ubuntu
 MAINTAINER blitterated blitterated@protonmail.com
 
 WORKDIR /root
-COPY bash_profile .bash_profile
-COPY bashrc .bashrc
-COPY provision/*.* provision/
+COPY shell/bash_profile .bash_profile
+COPY shell/bashrc .bashrc
+COPY shell/source_files.sh .source_files.sh
+COPY dde.rc/*.* .dde.rc/
+COPY provision/*.* .provision/
+
+RUN ./.provision/apt_prep.sh
+RUN ./.provision/manpages.sh
+RUN ./.provision/apt_install.sh
+RUN ./.provision/s6-overlay.sh
 
 COPY utils/container/bounce /usr/bin/bounce
 COPY utils/container/path /usr/bin/path
 COPY utils/container/docker-s6-quick-exit /usr/bin/docker-s6-quick-exit
-
-RUN ./provision/apt_prep.sh
-RUN ./provision/manpages.sh
-RUN ./provision/apt_install.sh
-RUN ./provision/s6-overlay.sh
 
 ENTRYPOINT ["/init"]
---
+++ shell/source_files.sh
@@ -0,0 +1,49 @@
+function log_ts {
+  echo "$(date +'%Y-%m-%d %T'): $1"
+}
+
+function source_files {
+
+  # Bail out if no directory argument given
+  if [ "$#" -lt 1 ]; then log_ts "No directory supplied"; return 1; fi
+
+  SOURCE_DIR=$1
+
+  # Check for existence of argument
+  if [ -e "${SOURCE_DIR}" ]; then
+    log_ts "'${SOURCE_DIR}' exists"
+  else
+    log_ts "'${SOURCE_DIR}' does not exist"
+    return 0
+  fi
+
+  # Check that it's a directory
+  if [ -d "${SOURCE_DIR}" ]; then
+    log_ts "'${SOURCE_DIR}' is a directory"
+  else
+    log_ts "'${SOURCE_DIR}' is not a directory"
+    return 0
+  fi
+
+  # Get a list of all files in the directory.
+  SOURCE_FILES=$(ls -q1A $SOURCE_DIR | sort)
+
+  # Check if the directory is empty or not
+  if [ ! -z "$SOURCE_FILES" ]; then
+    log_ts "'${SOURCE_DIR}' contains files"
+  else
+    log_ts "'${SOURCE_DIR}' is empty"
+    return 0
+  fi
+
+  # Loop over each file and source it
+  # Assume that they're all sourceable without error.
+  # Also, swap out the Input Field Separator setting (IFS) for a newline until the loop completes.
+  OLD_IFS=$IFS
+  IFS=$'\n'
+  for file in ${SOURCE_FILES}; do
+    log_ts "sourcing '${SOURCE_DIR}/${file}'"
+    source "${SOURCE_DIR}/${file}"
+  done
+  IFS=$OLD_IFS
+}
